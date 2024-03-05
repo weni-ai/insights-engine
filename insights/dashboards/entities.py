a568f4fa34eb6df0569f02bde61662f434317a96
helllllllder <heldergsds@gmail.com>
2024-03-05 14:18:36 -0300

    feat: add dashboard related entities

--- insights/dashboards/entities.py
+++ insights/dashboards/entities.py
@@ -1,10 +1,35 @@
-class Dashboard: ...
+from dataclasses import dataclass
+from typing import Optional
 
 
-class Widget: ...
+@dataclass
+class Dashboard:
+    uuid: str
+    project: str
+    description: str
+    config: dict
 
 
-class DashboardTemplate: ...
+@dataclass
+class Widget:
+    uuid: str
+    dashboard: str
+    description: str
+    w_type: str
+    source: str
+    report: dict
+    config: dict
 
 
-class WidgetTemplate: ...
+@dataclass
+class DashboardTemplate:
+    uuid: str
+    project: Optional[str] = None
+    setup: dict
+
+
+@dataclass
+class WidgetTemplate:
+    uuid: str
+    project: Optional[str]
+    setup: dict
