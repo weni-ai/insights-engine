30f2b29c9e6cf20d94f46258f52b346e9d8765f5
helllllllder <heldergsds@gmail.com>
2024-03-05 11:06:52 -0300

    feat: add project related entities

--- insights/projects/entities.py
+++ insights/projects/entities.py
@@ -1,4 +1,23 @@
-class Project: ...
+from dataclasses import dataclass
 
 
-class Permission: ...
+@dataclass
+class Project:
+    uuid: str = None
+    name: str = None
+    timezone: str = None
+    config: dict = None
+
+
+@dataclass
+class Permission:
+    uuid: str = None
+    user: str = None
+    role: str = None
+
+
+@dataclass
+class ChatSession:
+    uuid: str = None
+    permission: str = None
+    session_id: str = None
