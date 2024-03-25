53280dbc6dba968c813cfa01efaf7b45bc859b77
helllllllder <heldergsds@gmail.com>
2024-03-25 11:50:26 -0300

    feat: init setup after prototypes 🎉

---
+++ docker-entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+export GUNICORN_APP=${GUNICORN_APP:-"chats.wsgi"}
+export GUNICORN_CONF=${GUNICORN_CONF:-"${PROJECT_PATH}/gunicorn.conf.py"}
+export LOG_LEVEL=${LOG_LEVEL:-"INFO"}
+
+do_gosu(){
+    user="$1"
+    shift 1
+
+    is_exec="false"
+    if [ "$1" = "exec" ]; then
+        is_exec="true"
+        shift 1
+    fi
+
+    if [ "$(id -u)" = "0" ]; then
+        if [ "${is_exec}" = "true" ]; then
+            exec gosu "${user}" "$@"
+        else
+            gosu "${user}" "$@"
+            return "$?"
+        fi
+    else
+        if [ "${is_exec}" = "true" ]; then
+            exec "$@"
+        else
+            eval '"$@"'
+            return "$?"
+        fi
+    fi
+}
+
+
+if [[ "start" == "$1" ]]; then
+    echo "Run collectstatic"
+    do_gosu "${PROJECT_USER}:${PROJECT_GROUP}" python manage.py collectstatic --noinput
+    echo "Starting gunicorn workers"
+    do_gosu "${PROJECT_USER}:${PROJECT_GROUP}" exec gunicorn "${GUNICORN_APP}" \
+      --name="${APPLICATION_NAME}" \
+      --chdir="${PROJECT_PATH}" \
+      --bind=0.0.0.0:8000 \
+      -c "${GUNICORN_CONF}"
+fi
+
+exec "$@"
