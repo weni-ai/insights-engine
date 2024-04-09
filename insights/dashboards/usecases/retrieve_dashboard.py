432337f9de5e5de48a189a4285840bd98be0c508
helllllllder <heldergsds@gmail.com>
2024-04-09 17:55:09 -0300

    feat: start dashboard retrieve usecase

--- insights/dashboards/usecases/retrieve_dashboard.py
+++ insights/dashboards/usecases/retrieve_dashboard.py
@@ -1,2 +1,36 @@
+from dataclasses import dataclass
+
+from typing import List
+
 from insights.dashboards.models import Dashboard
-from insights.projects.usecases.create import ProjectsUseCase
+
+
+@dataclass
+class DashFiltersDTO:
+    project: str
+    uuid: str = None
+    name: str = None
+    description: str = None
+
+    def asdict(self):
+
+        return {k: str(v) for k, v in self.__dict__.items() if v is not None}
+
+
+class DashboardRetrieveUseCase:
+    def __init__(self, storage_manager=Dashboard.objects) -> None:
+        self.storage_manager = storage_manager
+
+    def get(self, pk):
+        return self.storage_manager.get(pk=pk)
+
+    def list(
+        self,
+        filters: DashFiltersDTO,
+        ordering: List[str] = [],
+    ):
+        query = self.storage_manager.filter(filters.asdict())
+        if ordering != []:
+            query = query.order_by(*ordering)
+
+        return query
