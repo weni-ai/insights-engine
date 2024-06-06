9c10d3cd306f5a88a63d11c61925856818bae04d
helllllllder <heldergsds@gmail.com>
2024-06-06 17:04:49 -0300

    feat: fix gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,8 @@
 import multiprocessing
 import os
 
-workers = os.environ.get("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1)proc_name = "insights-engine"
+workers = os.environ.get("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1)
+proc_name = "insights-engine"
 default_proc_name = proc_name
 accesslog = "-"
 errorlog = "-"
