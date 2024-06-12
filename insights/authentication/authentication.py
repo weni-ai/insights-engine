38afde7161965c1b473c320d9c60da14f8915e0a
helllllllder <heldergsds@gmail.com>
2024-06-12 15:42:40 -0300

    feat: add RoomRESTClient

--- insights/authentication/authentication.py
+++ insights/authentication/authentication.py
@@ -1,10 +1,30 @@
 import logging
+import re
 
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 
 from insights.users.usecases import CreateUserUseCase
 
 LOGGER = logging.getLogger("weni_django_oidc")
+
+User = get_user_model()
+
+
+def check_module_permission(claims, user):
+    if claims.get("can_communicate_internally", False):
+        content_type = ContentType.objects.get_for_model(User)
+        permission, created = Permission.objects.get_or_create(
+            codename="can_communicate_internally",
+            name="can communicate internally",
+            content_type=content_type,
+        )
+        if not user.has_perm("accounts.can_communicate_internally"):
+            user.user_permissions.add(permission)
+        return True
+    return False
 
 
 class WeniOIDCAuthenticationBackend(OIDCAuthenticationBackend):
@@ -18,14 +38,17 @@ class WeniOIDCAuthenticationBackend(OIDCAuthenticationBackend):
             return username
         return super(WeniOIDCAuthenticationBackend, self).get_username(claims=claims)
 
-    def create_user(self, claims):
-        # Override existing create_user method in OIDCAuthenticationBackend
-        email = claims.get("email")
-        return CreateUserUseCase().create_user(email)
-
     def update_user(self, user, claims):
         user.name = claims.get("name", "")
         user.email = claims.get("email", "")
         user.save()
+
+        return user
+
+    def create_user(self, claims):
+        email = claims.get("email")
+        user = CreateUserUseCase().create_user(email)
+
+        check_module_permission(claims, user)
 
         return user
--- insights/sources/rooms/usecases/query_execute.py
+++ insights/sources/rooms/usecases/query_execute.py
@@ -7,12 +7,22 @@
 # )
 
 from insights.db.postgres.connection import get_cursor
-from insights.sources.rooms.clients import generate_sql_query
+from insights.sources.rooms.clients import RoomRESTClient, generate_sql_query
 
 
 class QueryExecutor:
-    def execute(filters: dict, action: str, parser: callable, *args, **kwargs):
-        query, params = generate_sql_query(filters=filters, query_type=action)
+    def execute(
+        filters: dict, action: str, parser: callable, project: object, *args, **kwargs
+    ):
+        if action == "list":
+            client = RoomRESTClient(project=project)
+            query_results = client.list(filters)
+            paginated_results = {
+                "next": query_results.get("next").split("?")[1],
+                "previous": query_results.get("previous").split("?")[1],
+                "results": query_results.get("results"),
+            }
+            query, params = generate_sql_query(filters=filters, query_type=action)
         with get_cursor(db_name="chats") as cur:
             query_results = cur.execute(query, params).fetchall()
         paginated_results = {"next": None, "previous": None, "results": query_results}
