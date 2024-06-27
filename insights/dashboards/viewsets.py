01481e51b6d7ce860c4664308ac09a6e2a4a1053
helllllllder <heldergsds@gmail.com>
2024-06-27 11:18:26 -0300

    feat: remove exclude from dashboards

--- insights/dashboards/viewsets.py
+++ insights/dashboards/viewsets.py
@@ -34,10 +34,7 @@ class DashboardViewSet(
         if project_id is not None:
             return (
                 Dashboard.objects.filter(project_id=project_id)
-                .exclude(
-                    Q(name="Resultados de fluxos")
-                    & ~Q(project_id__in=settings.PROJECT_ALLOW_LIST)
-                )
+                .exclude(~Q(project_id__in=settings.PROJECT_ALLOW_LIST))
                 .order_by("created_on")
             )
 
