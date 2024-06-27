6c5880f6286efe51f97a46d39efb2a4fa63b62bd
helllllllder <heldergsds@gmail.com>
2024-06-27 14:56:15 -0300

    feat: fix widget serializer

--- insights/widgets/serializers.py
+++ insights/widgets/serializers.py
@@ -4,7 +4,7 @@ from .models import Widget
 
 
 class WidgetSerializer(serializers.ModelSerializer):
-    is_configurable = serializers.BooleanField(source="is_configurable", read_only=True)
+    is_configurable = serializers.BooleanField(read_only=True)
 
     class Meta:
         model = Widget
