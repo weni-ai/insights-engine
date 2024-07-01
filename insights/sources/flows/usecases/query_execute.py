a9b3ef04114cd69de24101734d8d6f9f704014e2
helllllllder <heldergsds@gmail.com>
2024-07-01 15:31:19 -0300

    feat: make flows db connection with psycopg manually instead of using django connections

--- insights/sources/flows/usecases/query_execute.py
+++ insights/sources/flows/usecases/query_execute.py
@@ -1,4 +1,4 @@
-from insights.db.postgres.connection import dictfetchall, get_cursor
+from insights.db.postgres.psycopg.connection import get_cursor
 from insights.sources.flows.clients import generate_sql_query
 
 
@@ -14,7 +14,6 @@ class QueryExecutor:
         filters["project"] = str(project.uuid)
         query, params = generate_sql_query(filters=filters, query_type=operation)
         with get_cursor(db_name="flows") as cur:
-            query_exec = cur.execute(query, params)
-            query_results = dictfetchall(query_exec)
+            query_results = cur.execute(query, params).fetchall()
         paginated_results = {"next": None, "previous": None, "results": query_results}
         return paginated_results  # parser(paginated_results)
