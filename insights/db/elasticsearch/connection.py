1d8babfe6ccec1b2d653a7b2caef535045f7125f
Helder Souza <42891390+helllllllder@users.noreply.github.com>
2024-07-02 11:17:44 -0300

    Feature/add metadata field to flow (#19)
    * feat: add metadata field on the list flows query
    
    * feat: fix elasticsearch connection

--- insights/db/elasticsearch/connection.py
+++ insights/db/elasticsearch/connection.py
@@ -7,4 +7,4 @@ class Connection:
         self.base_url = settings.FLOWS_ES_DATABASE + endpoint
 
     def get(self, params: dict):
-        return requests.get(url=self.base_url, params=params).json()
+        return requests.get(url=self.base_url, json=params).json()
