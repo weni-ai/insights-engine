2797feac050cce9df15f5cc3be5b6c49c63129a8
helllllllder <heldergsds@gmail.com>
2024-07-02 11:16:47 -0300

    feat: fix elasticsearch connection

--- insights/sources/flowruns/clients.py
+++ insights/sources/flowruns/clients.py
@@ -6,6 +6,7 @@ from insights.sources.flowruns.query_builder import (
 flow_runs_filters = {
     "created_on": {"type": "date", "to_field": "created_on"},
     "exited_on": {"type": "date", "to_field": "exited_on"},
+    "ended_at": {"type": "date", "to_field": "exited_on"},
     "project": {"type": "string", "to_field": "project_uuid"},
     "flow": {"type": "string", "to_field": "flow_uuid"},
 }
