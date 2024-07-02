baabdc5b0ed9cdf241d8416394067b704ee8fb95
AlanJaeger <aland295@gmail.com>
2024-07-02 17:12:29 -0300

    feature: adding isoformat to date with timezone

--- insights/sources/agents/clients.py
+++ insights/sources/agents/clients.py
@@ -15,9 +15,9 @@ class AgentsRESTClient(InternalAuthentication):
 
     def list(self, query_filters: dict):
         if query_filters.get("created_on__gte", None):
-            query_filters["start_date"] = query_filters.pop("created_on__gte")[0]
+            query_filters["start_date"] = query_filters.pop("created_on__gte")
         if query_filters.get("created_on__lte", None):
-            query_filters["end_date"] = query_filters.pop("created_on__lte")[0]
+            query_filters["end_date"] = query_filters.pop("created_on__lte")
 
         response = requests.get(
             url=self.url, headers=self.headers, params=query_filters
