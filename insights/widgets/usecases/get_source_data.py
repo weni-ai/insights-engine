d19e20d4055fa434899acfabd844795253707f2b
AlanJaeger <aland295@gmail.com>
2024-08-21 16:30:13 -0300

    feature: adding comments in default filters

--- insights/widgets/usecases/get_source_data.py
+++ insights/widgets/usecases/get_source_data.py
@@ -74,7 +74,7 @@ def simple_source_data_operation(
     # se entrar aqui pelo cross data, tirar o [0] pois isso gera erro no cross
     # vc pode verificar se tem slug subwidget_1 or 2
     sub = filters.pop("slug", [None])
-    if sub in ["sub_widget_1", "sub_widget_2"]:
+    if sub in ["subwidget_1", "subwidget_2"]:
         default_filters, operation, op_field, op_sub_field, limit = (
             widget.source_config(
                 sub_widget=filters.pop("slug", [None]), is_live=is_live
@@ -87,8 +87,8 @@ def simple_source_data_operation(
                 sub_widget=filters.pop("slug", [None])[0], is_live=is_live
             )
         )
-        print("default filters", default_filters)
 
+    print("default filters", default_filters)
     default_filters.update(filters)
 
     if is_live:
