c5052986d9e58de4b4ba6a24c0eaf9080940997a
AlanJaeger <aland295@gmail.com>
2024-10-17 15:27:49 -0300

    debuging error

--- insights/sources/vtexcredentials/clients.py
+++ insights/sources/vtexcredentials/clients.py
@@ -9,11 +9,15 @@ class AuthRestClient(InternalAuthentication):
         self.url = f"{settings.INTEGRATIONS_URL}/api/v1/apptypes/vtex/integration-details/{project}"
 
     def get_vtex_auth(self):
-        response = requests.get(url=self.url, headers=self.headers)
-        tokens = response.json()
-        credentials = {
-            "app_key": tokens["app_key"],
-            "app_token": tokens["app_token"],
-            "domain": tokens["domain"],
-        }
-        return credentials
+        # response = requests.get(url=self.url, headers=self.headers)
+        # print("url", self.url)
+        # print("RESPONSE", response)
+        # tokens = response.json()
+        # print("TOKEN", tokens)
+        # credentials = {
+        #     "app_key": tokens["app_key"],
+        #     "app_token": tokens["app_token"],
+        #     "domain": tokens["domain"],
+        # }
+        # print("credenciais")
+        return {}
