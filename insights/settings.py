63716e3a0b742356fe884ccee246e67370b265fc
AlanJaeger <aland295@gmail.com>
2024-10-17 15:53:04 -0300

    adding mock

--- insights/settings.py
+++ insights/settings.py
@@ -291,9 +291,9 @@ CACHES = {
     }
 }
 INTEGRATIONS_URL = "https://integrations-engine.weni.ai"
-MOCKDOMAIN = (env.str("MOCKDOMAIN"),)
-MOCK_APPKEY = (env.str("MOCK_APPKEY"),)
-MOCK_APPTOKEN = (env.str("MOCK_APPTOKEN"),)
+MOCKDOMAIN = env.str("MOCKDOMAIN")
+MOCK_APPKEY = env.str("MOCK_APPKEY")
+MOCK_APPTOKEN = env.str("MOCK_APPTOKEN")
 
 GROQ_OPEN_AI_URL = env.str("GROQ_OPEN_AI_URL", default="")
 GROQ_CHATGPT_TOKEN = env.str("GROQ_CHATGPT_TOKEN", default="")
