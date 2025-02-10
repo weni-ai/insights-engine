dd6927c2487741155742829719c2669cf34d4f9e
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-02-10 17:18:35 -0300

    feat: add tests for vtex orders view

---
+++ insights/metrics/vtex/tests/test_orders_view.py
@@ -0,0 +1,96 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.test import APITestCase
+from unittest.mock import patch
+
+from insights.authentication.authentication import User
+from insights.authentication.tests.decorators import with_project_auth
+from insights.projects.models import Project
+
+
+class BaseTestVtexOrdersView(APITestCase):
+    base_url = "/v1/metrics/vtex/orders/"
+
+    def get_utm_revenue(self, query_params: dict | None = None) -> Response:
+        url = self.base_url + "utm_revenue/"
+
+        return self.client.get(url, query_params)
+
+
+class TestVtexOrdersViewAsAnonymousUser(BaseTestVtexOrdersView):
+    def test_get_utm_revenue_without_project_uuid(self):
+        response = self.get_utm_revenue()
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class TestVtexOrdersViewAsAuthenticatedUser(BaseTestVtexOrdersView):
+    def setUp(self):
+        self.user = User.objects.create()
+        self.project = Project.objects.create(name="Test Project")
+
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_utm_revenue_without_permission(self):
+        response = self.get_utm_revenue()
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_cannot_get_utm_revenue_without_project_uuid(self):
+        response = self.get_utm_revenue()
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    @with_project_auth
+    @patch("insights.sources.orders.clients.VtexOrdersRestClient.list")
+    def test_cannot_get_utm_revenue_without_feature(self, mock_list):
+        mock_list.return_value = {
+            "countSell": 1,
+            "accumulatedTotal": 50.21,
+            "ticketMax": 50.21,
+            "ticketMin": 50.21,
+            "medium_ticket": 50.21,
+        }
+
+        query_params = {
+            "project_uuid": self.project.uuid,
+            "ended_at__gte": "2023-09-01T00:00:00.000Z",
+            "ended_at__lte": "2023-09-04T00:00:00.000Z",
+        }
+
+        response = self.get_utm_revenue(query_params)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["feature"][0].code, "missing_feature")
+
+    @with_project_auth
+    @patch("insights.sources.vtexcredentials.clients.AuthRestClient.get_vtex_auth")
+    @patch("insights.sources.orders.clients.VtexOrdersRestClient.list")
+    def test_get_utm_revenue(self, mock_list, mock_get_vtex_auth):
+        expected_utm_revenue = 50.21
+
+        mock_list.return_value = {
+            "countSell": 1,
+            "accumulatedTotal": expected_utm_revenue,
+            "ticketMax": 50.21,
+            "ticketMin": 50.21,
+            "medium_ticket": 50.21,
+        }
+        mock_get_vtex_auth.return_value = {
+            "app_token": "fake_token",
+            "app_key": "fake_key",
+            "domain": "fake_domain",
+        }
+
+        query_params = {
+            "project_uuid": self.project.uuid,
+            "feature": "abandoned_cart",
+            "ended_at__gte": "2023-09-01T00:00:00.000Z",
+            "ended_at__lte": "2023-09-04T00:00:00.000Z",
+        }
+
+        response = self.get_utm_revenue(query_params)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("utm_revenue", response.data)
+        self.assertEqual(response.data["utm_revenue"], expected_utm_revenue)
