605db4e0efdfd70fed52e575db26d7e79376a84e
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-02-17 16:41:50 -0300

    feat: add required params in whatsapp message template endpoints schemas

---
+++ insights/metrics/meta/schema.py
@@ -0,0 +1,43 @@
+from drf_spectacular.openapi import OpenApiParameter
+
+
+WHATSAPP_MESSAGE_TEMPLATES_GENERAL_PARAMS = [
+    OpenApiParameter(
+        name="project_uuid",
+        type=str,
+        location=OpenApiParameter.QUERY,
+        required=True,
+    ),
+    OpenApiParameter(
+        name="waba_id",
+        type=str,
+        location=OpenApiParameter.QUERY,
+        required=True,
+    ),
+    OpenApiParameter(
+        name="template_id",
+        type=str,
+        location=OpenApiParameter.QUERY,
+        required=True,
+    ),
+]
+
+WHATSAPP_MESSAGE_TEMPLATES_MSGS_ANALYTICS_PARAMS = (
+    WHATSAPP_MESSAGE_TEMPLATES_GENERAL_PARAMS
+    + [
+        OpenApiParameter(
+            name="start_date",
+            type=str,
+            location=OpenApiParameter.QUERY,
+            required=True,
+            description="Date in YYYY-MM-DD format",
+        ),
+        OpenApiParameter(
+            name="end_date",
+            type=str,
+            location=OpenApiParameter.QUERY,
+            required=True,
+            description="Date in YYYY-MM-DD format",
+        ),
+    ]
+)
