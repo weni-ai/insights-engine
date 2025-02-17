4b9b9c3c6a07778bc4421651a4e56a61be7f28b1
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-02-17 15:20:15 -0300

    feat: add project waba permission

---
+++ insights/metrics/meta/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework.permissions import BasePermission
+
+from insights.sources.wabas.clients import WeniIntegrationsClient
+
+
+class ProjectWABAPermission(BasePermission):
+    """
+    Permission to check if the user has access to the waba_id
+    """
+
+    def has_permission(self, request, view):
+        """
+        Check if the user has access to the waba_id
+        """
+        project_uuid = request.query_params.get("project_uuid")
+        waba_id = request.query_params.get("waba_id")
+
+        integrations_client = WeniIntegrationsClient(project_uuid=project_uuid)
+        wabas_data = integrations_client.get_wabas_for_project()
+        wabas_ids = {waba.get("waba_id") for waba in wabas_data}
+
+        return waba_id in wabas_ids
