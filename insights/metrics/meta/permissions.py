475d3ed17c87694f25aa01e3cae68ab14e42daa1
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-02-17 15:21:10 -0300

    feat: check weather project uuid and waba id were used in the query params for project waba permission

--- insights/metrics/meta/permissions.py
+++ insights/metrics/meta/permissions.py
@@ -15,6 +15,9 @@ class ProjectWABAPermission(BasePermission):
         project_uuid = request.query_params.get("project_uuid")
         waba_id = request.query_params.get("waba_id")
 
+        if not project_uuid or not waba_id:
+            return False
+
         integrations_client = WeniIntegrationsClient(project_uuid=project_uuid)
         wabas_data = integrations_client.get_wabas_for_project()
         wabas_ids = {waba.get("waba_id") for waba in wabas_data}
