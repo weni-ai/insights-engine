496fcf0f2311e5c5146b3365e2b33aff8fa5ad82
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-03-10 17:34:17 -0300

    feat: add vtex_account field to Project model

--- insights/projects/tests/test_usecases/test_create_project.py
+++ insights/projects/tests/test_usecases/test_create_project.py
@@ -2,18 +2,38 @@ from uuid import uuid4
 
 import pytest
 
+from django.test import TestCase
+
 from insights.projects.usecases.create import ProjectsUseCase
 from insights.projects.usecases.project_dto import ProjectCreationDTO
 
 
-@pytest.mark.django_db
-def test_create_project():
-    project_dto = ProjectCreationDTO(
-        uuid=uuid4().hex,
-        name="test_name",
-        timezone="America/Bahia",
-        date_format="DD/MM/YYYY",
-        is_template=False,
-    )
-    project = ProjectsUseCase().create_project(project_dto=project_dto)
-    assert project.uuid == project_dto.uuid
+class TestCreateProjectUseCase(TestCase):
+    def test_create_project(self):
+        project_dto = ProjectCreationDTO(
+            uuid=uuid4().hex,
+            name="test_name",
+            timezone="America/Bahia",
+            date_format="DD/MM/YYYY",
+            is_template=False,
+        )
+
+        project = ProjectsUseCase().create_project(project_dto=project_dto)
+
+        self.assertEqual(project.uuid, project_dto.uuid)
+        self.assertIsNone(project.vtex_account)
+
+    def test_create_project_with_vtex_account(self):
+        project_dto = ProjectCreationDTO(
+            uuid=uuid4().hex,
+            name="test_name",
+            timezone="America/Bahia",
+            date_format="DD/MM/YYYY",
+            is_template=False,
+            vtex_account="test_vtex_account",
+        )
+
+        project = ProjectsUseCase().create_project(project_dto=project_dto)
+
+        self.assertEqual(project.uuid, project_dto.uuid)
+        self.assertEqual(project.vtex_account, project_dto.vtex_account)
