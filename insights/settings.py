d25ef5174f448d95b9fcbf9fd198bd7b7d2490ed
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-03-18 10:44:29 -0300

    feat: add UserLanguageMiddleware to manage user language preferences

--- insights/settings.py
+++ insights/settings.py
@@ -82,6 +82,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "insights.middleware.UserLanguageMiddleware",
 ]
 
 ROOT_URLCONF = "insights.urls"
