9c9541bc70d6faed7adf9de1a122995a20f6787e
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-04-02 09:49:42 -0300

    refactor: remove debug print statements from QueryExecutor

--- insights/sources/vtex_conversions/usecases/query_execute.py
+++ insights/sources/vtex_conversions/usecases/query_execute.py
@@ -5,8 +5,6 @@ from insights.sources.vtex_conversions.services import VTEXOrdersConversionsServ
 class QueryExecutor:
     @staticmethod
     def execute(filters: dict, *args, project: Project, **kwargs):
-        print("VTEX CONVERSIONS QUERY EXECUTOR - filters")
-        print(filters)
         service = VTEXOrdersConversionsService(project)
 
         return service.get_metrics(filters)
