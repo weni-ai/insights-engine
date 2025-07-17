e12907e11569a79195302632c711ac9bf71c4648
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-07-17 11:39:38 -0300

    Update weni-datalake-sdk to version 0.3.0a0 and refactor DataLakeEventsClient
    - Updated the version of `weni-datalake-sdk` in `poetry.lock` and `pyproject.toml` to 0.3.0a0.
    - Refactored `DataLakeEventsClient` to inherit from `BaseDataLakeEventsClient`, introducing an abstract method for `get_events_count` to enhance functionality and maintainability.

--- insights/sources/dl_events/clients.py
+++ insights/sources/dl_events/clients.py
@@ -1,17 +1,49 @@
-from weni_datalake_sdk.clients.redshift.events import get_events
+from abc import ABC, abstractmethod
+from weni_datalake_sdk.clients.redshift.events import get_events, get_events_count
 
 
-class DataLakeEventsClient:
+class BaseDataLakeEventsClient(ABC):
+    """
+    Base class for the DataLakeEventsClient.
+    """
+
+    @abstractmethod
+    def get_events(self, query_kwargs: dict) -> dict | list[dict]:
+        """
+        Get events from the DataLakeEvents source.
+        """
+
+    @abstractmethod
+    def get_events_count(self, **query_kwargs) -> dict:
+        """
+        Get the count of events from the DataLakeEvents source.
+        """
+
+
+class DataLakeEventsClient(BaseDataLakeEventsClient):
     """
     Client for the DataLakeEvents source.
     """
 
-    def get_events(self, query_kwargs: dict) -> dict | list[dict]:
+    def get_events(self, **query_kwargs) -> dict | list[dict]:
         """
         Get events from the DataLakeEvents source.
         """
         try:
             events = get_events(
+                **query_kwargs,
+            )
+        except Exception as e:
+            raise e
+
+        return events
+
+    def get_events_count(self, **query_kwargs) -> dict:
+        """
+        Get the count of events from the DataLakeEvents source.
+        """
+        try:
+            events = get_events_count(
                 **query_kwargs,
             )
         except Exception as e:
