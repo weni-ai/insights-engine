a5813f60ab2bcf37b82d68a1bc56d8ccae9049b6
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-07-29 14:53:36 -0300

    Add tests for widget viewset with authentication and permission checks
    - Introduced a new test class for authenticated users in TestWidgetViewSet.
    - Implemented tests to verify widget listing behavior based on user permissions.
    - Ensured that widgets are only listed for users with the appropriate project access.
    - Created helper methods for widget creation and authentication setup.

--- insights/widgets/tests/test_widget_viewset.py
+++ insights/widgets/tests/test_widget_viewset.py
@@ -5,6 +5,12 @@ from rest_framework.test import APITestCase
 from rest_framework.response import Response
 from rest_framework import status
 
+from insights.authentication.authentication import User
+from insights.authentication.tests.decorators import with_project_auth
+from insights.dashboards.models import Dashboard
+from insights.projects.models import Project
+from insights.widgets.models import Widget
+
 
 class BaseTestWidgetViewSet(APITestCase):
     def create_widget(self, data: dict) -> Response:
@@ -48,3 +54,46 @@ class TestWidgetViewSetAsAnonymousUser(BaseTestWidgetViewSet):
         response = self.delete_widget(uuid.uuid4())
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class TestWidgetViewSetAsAuthenticationUser(BaseTestWidgetViewSet):
+    def setUp(self):
+        self.user = User.objects.create_user(email="test@email.com")
+        self.project = Project.objects.create(
+            name="testproject",
+        )
+        self.dashboard = Dashboard.objects.create(
+            name="testdashboard",
+            project=self.project,
+        )
+
+        self.client.force_authenticate(self.user)
+
+    def _create_widget(self):
+        return Widget.objects.create(
+            name="testwidget",
+            dashboard=self.dashboard,
+            source="test",
+            position=[],
+            config={},
+            type="test",
+        )
+
+    def test_list_widgets_without_permission(self):
+        widget = self._create_widget()
+        response = self.list_widgets()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotIn(
+            str(widget.uuid), {widget["uuid"] for widget in response.data["results"]}
+        )
+
+    @with_project_auth
+    def test_list_widgets_with_permission(self):
+        widget = self._create_widget()
+        response = self.list_widgets()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn(
+            str(widget.uuid), {widget["uuid"] for widget in response.data["results"]}
+        )
