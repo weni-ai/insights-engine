ce0f33c355d4955f7f63e14cfcac37a746b02f64
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-08-12 11:26:16 -0300

    Add tests for ProjectIndexerActivation queue management
    - Implemented unit tests in `test_indexer_activation_service.py` to validate the behavior of adding projects to the activation queue.
    - Added scenarios to check project status when not allowed, when allowed, and when already in the queue.
    - Ensured that the tests cover the expected outcomes for each case, enhancing the reliability of the ProjectIndexerActivationService.

--- insights/projects/tests/test_services/test_indexer_activation_service.py
+++ insights/projects/tests/test_services/test_indexer_activation_service.py
@@ -2,7 +2,8 @@ import uuid
 
 from django.test import TestCase, override_settings
 
-from insights.projects.models import Project
+from insights.projects.choices import ProjectIndexerActivationStatus
+from insights.projects.models import Project, ProjectIndexerActivation
 from insights.projects.services.indexer_activation import (
     ProjectIndexerActivationService,
 )
@@ -33,3 +34,32 @@ class TestIndexerActivationService(TestCase):
         self,
     ):
         self.assertTrue(self.service.is_project_active_on_indexer(self.project))
+
+    def test_add_project_to_queue_when_project_is_not_allowed_and_not_in_queue(self):
+        self.assertFalse(
+            ProjectIndexerActivation.objects.filter(
+                project=self.project,
+                status=ProjectIndexerActivationStatus.PENDING,
+            ).exists()
+        )
+
+        self.assertTrue(self.service.add_project_to_queue(self.project))
+
+        self.assertTrue(
+            ProjectIndexerActivation.objects.filter(
+                project=self.project,
+                status=ProjectIndexerActivationStatus.PENDING,
+            ).exists()
+        )
+
+    def test_add_project_to_queue_when_project_is_allowed(self):
+        self.project.is_allowed = True
+        self.project.save(update_fields=["is_allowed"])
+        self.assertFalse(self.service.add_project_to_queue(self.project))
+
+    def test_add_project_to_queue_when_project_is_in_queue(self):
+        ProjectIndexerActivation.objects.create(
+            project=self.project,
+            status=ProjectIndexerActivationStatus.PENDING,
+        )
+        self.assertFalse(self.service.add_project_to_queue(self.project))
