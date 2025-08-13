bd1ae5b5348de39e192e21e1a76c9fa4f098dbb2
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-08-13 10:44:48 -0300

    Add Celery worker and beat commands to Docker entrypoint
    - Introduced environment variables for configuring Celery worker and beat services.
    - Added logic to handle starting Celery worker and beat processes in the Docker entrypoint script.
    - Set default values for maximum workers and beat database file location.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,8 @@
 export GUNICORN_APP=${GUNICORN_APP:-"insights.wsgi"}
 export GUNICORN_CONF=${GUNICORN_CONF:-"${PROJECT_PATH}/gunicorn.conf.py"}
 export LOG_LEVEL=${LOG_LEVEL:-"INFO"}
+export CELERY_MAX_WORKERS=${CELERY_MAX_WORKERS:-'4'}
+export CELERY_BEAT_DATABASE_FILE=${CELERY_BEAT_DATABASE_FILE:-'/tmp/celery_beat_database'}
 
 do_gosu(){
     user="$1"
@@ -41,6 +43,22 @@ if [[ "start" == "$1" ]]; then
       --chdir="${PROJECT_PATH}" \
       --bind=0.0.0.0:8000 \
       -c "${GUNICORN_CONF}"
+elif [[ "celery-worker" == "$1" ]]; then
+    celery_queue="celery"
+    if [ "${2}" ] ; then
+        celery_queue="${2}"
+    fi
+    do_gosu "${PROJECT_USER}:${PROJECT_GROUP}" exec celery \
+        -A insights --workdir="${PROJECT_PATH}" worker \
+        -Q "${celery_queue}" \
+        -O fair \
+        -l "${LOG_LEVEL}" \
+        --autoscale="${CELERY_MAX_WORKERS},1"
+elif [[ "celery-beat" == "$1" ]]; then
+    do_gosu "${PROJECT_USER}:${PROJECT_GROUP}" exec celery \
+        -A insights --workdir="${PROJECT_PATH}" beat \
+        --loglevel="${LOG_LEVEL}" \
+        -s "${CELERY_BEAT_DATABASE_FILE}"
 fi
 
 exec "$@"
