eb8782f855fa632b9282b8cbcaac541d5c64f0d8
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-10-02 08:25:12 -0300

    Add staging mock configurations and implement mock data responses in MetaMessageTemplatesService
    - Introduced new environment variables for staging mock dashboards in settings.py.
    - Enhanced MetaMessageTemplatesService to return mock data for templates, template previews, messages analytics, and button analytics based on the staging WABA IDs.

--- insights/metrics/meta/services.py
+++ insights/metrics/meta/services.py
@@ -5,6 +5,7 @@ from insights.metrics.meta.validators import (
     validate_analytics_kwargs,
     validate_list_templates_filters,
 )
+from django.conf import settings
 
 
 class MetaMessageTemplatesService:
@@ -22,6 +23,28 @@ class MetaMessageTemplatesService:
 
         valid_filters = validate_list_templates_filters(filters)
 
+        # [STAGING] Mock list templates
+        waba_id = valid_filters.get("waba_id")
+        if waba_id in settings.STG_MOCK_META_WABA_IDS:
+            return {
+                "data": [
+                    {
+                        "name": "Example",
+                        "parameter_format": "POSITIONAL",
+                        "components": [
+                            {
+                                "type": "BODY",
+                                "text": "Hello, {{1}}, this is an example",
+                            }
+                        ],
+                        "language": "en_US",
+                        "status": "APPROVED",
+                        "category": "MARKETING",
+                        "id": "123456789098765",
+                    }
+                ]
+            }
+
         return self.client.get_templates_list(**valid_filters)
 
     def get_template_preview(self, filters: dict):
@@ -34,6 +57,33 @@ class MetaMessageTemplatesService:
                 {"error": "'template_id' is required"},
                 code="template_id_missing",
             )
+
+        # [STAGING] Mock template preview
+        waba_id = filters.get("waba_id")
+        if waba_id in settings.STG_MOCK_META_WABA_IDS:
+            return {
+                "is_favorite": False,
+                "edit_template_url": None,
+                "name": "Example",
+                "parameter_format": "POSITIONAL",
+                "components": [
+                    {
+                        "type": "BODY",
+                        "text": "Hello, {{1}}, this is an example",
+                    },
+                    {
+                        "type": "BUTTONS",
+                        "buttons": [
+                            {"type": "QUICK_REPLY", "text": "Ok"},
+                            {"type": "QUICK_REPLY", "text": "Cancel"},
+                        ],
+                    },
+                ],
+                "language": "en_US",
+                "status": "APPROVED",
+                "category": "MARKETING",
+                "id": "123456789098765",
+            }
 
         return self.client.get_template_preview(template_id=template_id)
 
@@ -56,6 +106,71 @@ class MetaMessageTemplatesService:
         else:
             valid_filters = filters
 
+        # [STAGING] Mock messages analytics
+        waba_id = filters.get("waba_id")
+        if waba_id in settings.STG_MOCK_META_WABA_IDS:
+            return {
+                "data": {
+                    "status_count": {
+                        "sent": {"value": 269},
+                        "delivered": {"value": 268, "percentage": 99.63},
+                        "read": {"value": 203, "percentage": 75.46},
+                        "clicked": {"value": 177, "percentage": 65.8},
+                    },
+                    "data_points": [
+                        {
+                            "date": "2025-09-26",
+                            "sent": 0,
+                            "delivered": 0,
+                            "read": 0,
+                            "clicked": 0,
+                        },
+                        {
+                            "date": "2025-09-27",
+                            "sent": 0,
+                            "delivered": 0,
+                            "read": 0,
+                            "clicked": 0,
+                        },
+                        {
+                            "date": "2025-09-28",
+                            "sent": 0,
+                            "delivered": 0,
+                            "read": 0,
+                            "clicked": 0,
+                        },
+                        {
+                            "date": "2025-09-29",
+                            "sent": 0,
+                            "delivered": 0,
+                            "read": 0,
+                            "clicked": 0,
+                        },
+                        {
+                            "date": "2025-09-30",
+                            "sent": 0,
+                            "delivered": 0,
+                            "read": 0,
+                            "clicked": 0,
+                        },
+                        {
+                            "date": "2025-10-01",
+                            "sent": 269,
+                            "delivered": 268,
+                            "read": 201,
+                            "clicked": 172,
+                        },
+                        {
+                            "date": "2025-10-02",
+                            "sent": 0,
+                            "delivered": 0,
+                            "read": 2,
+                            "clicked": 5,
+                        },
+                    ],
+                }
+            }
+
         return self.client.get_messages_analytics(
             **valid_filters,
             include_data_points=include_data_points,
@@ -68,5 +183,25 @@ class MetaMessageTemplatesService:
         """
 
         valid_filters = validate_analytics_kwargs(filters, timezone_name=timezone_name)
+
+        # [STAGING] Mock buttons analytics
+        waba_id = filters.get("waba_id")
+        if waba_id in settings.STG_MOCK_META_WABA_IDS:
+            return {
+                "data": [
+                    {
+                        "label": "Ok",
+                        "type": "QUICK_REPLY",
+                        "total": 143,
+                        "click_rate": 53.16,
+                    },
+                    {
+                        "label": "Cancel",
+                        "type": "QUICK_REPLY",
+                        "total": 34,
+                        "click_rate": 12.64,
+                    },
+                ]
+            }
 
         return self.client.get_buttons_analytics(**valid_filters)
