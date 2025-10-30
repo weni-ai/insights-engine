802dad2db43d89a8ffad3c2d41380325a9eff7d5
AlanJaeger <aland295@gmail.com>
2025-10-30 15:34:00 -0300

    resolving date filter problems

--- insights/human_support/filters.py
+++ insights/human_support/filters.py
@@ -13,8 +13,8 @@ class HumanSupportFilterSet(filters.FilterSet):
     sectors = UUIDInFilter(required=False)
     queues = UUIDInFilter(required=False)
     tags = UUIDInFilter(required=False)
-    start_date = filters.IsoDateTimeFilter(required=False)
-    end_date = filters.IsoDateTimeFilter(required=False)
+    start_date = filters.DateFilter(required=False)
+    end_date = filters.DateFilter(required=False)
     agent = filters.UUIDFilter(required=False)
     contact = filters.UUIDFilter(required=False)
     ticket_id = filters.UUIDFilter(required=False)
@@ -42,18 +42,22 @@ class HumanSupportFilterSet(filters.FilterSet):
             
             if self.form.cleaned_data.get("start_date"):
                 start_date = self.form.cleaned_data["start_date"]
-                if start_date.tzinfo is not None:
-                    start_date = start_date.replace(tzinfo=None)
+                if isinstance(start_date, datetime):
+                    if start_date.tzinfo is not None:
+                        start_date = start_date.replace(tzinfo=None)
+                    start_date = start_date.date()
                 
-                start_datetime = datetime.combine(start_date.date(), time.min)
+                start_datetime = datetime.combine(start_date, time.min)
                 self.form.cleaned_data["start_date"] = timezone.localize(start_datetime)
             
             if self.form.cleaned_data.get("end_date"):
                 end_date = self.form.cleaned_data["end_date"]
-                if end_date.tzinfo is not None:
-                    end_date = end_date.replace(tzinfo=None)
+                if isinstance(end_date, datetime):
+                    if end_date.tzinfo is not None:
+                        end_date = end_date.replace(tzinfo=None)
+                    end_date = end_date.date()
                 
-                end_datetime = datetime.combine(end_date.date(), time(23, 59, 59))
+                end_datetime = datetime.combine(end_date, time(23, 59, 59))
                 self.form.cleaned_data["end_date"] = timezone.localize(end_datetime)
             
             return self.form.cleaned_data
