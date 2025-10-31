c9a55a3287e6e2ab52741f968191a4720f71aa96
alandavl <aland295@gmail.com>
2025-10-31 16:01:00 -0300

    Feature/v2 human dashboard insights (#224)
    * feature: v2 version of new human dashboard

--- insights/metrics/human_support/views.py
+++ insights/metrics/human_support/views.py
@@ -23,7 +23,8 @@ class DetailedMonitoringOnGoingView(APIView):
         project = get_object_or_404(Project, uuid=project_uuid)
         service = HumanSupportDashboardService(project=project)
 
-        data = service.get_detailed_monitoring_on_going(filters=request.query_params)
+        filters = {key: value for key, value in request.query_params.items()}
+        data = service.get_detailed_monitoring_on_going(filters=filters)
         return Response(data, status=200)
 
 
@@ -39,7 +40,8 @@ class DetailedMonitoringAwaitingView(APIView):
         project = get_object_or_404(Project, uuid=project_uuid)
         service = HumanSupportDashboardService(project=project)
 
-        data = service.get_detailed_monitoring_awaiting(filters=request.query_params)
+        filters = {key: value for key, value in request.query_params.items()}
+        data = service.get_detailed_monitoring_awaiting(filters=filters)
         return Response(data, status=200)
 
 
@@ -55,7 +57,7 @@ class DetailedMonitoringAgentsView(APIView):
         project = get_object_or_404(Project, uuid=project_uuid)
         service = HumanSupportDashboardService(project=project)
 
-        filters = request.query_params.copy()
+        filters = {key: value for key, value in request.query_params.items()}
         filters["user_request"] = request.user.email
         data = service.get_detailed_monitoring_agents(filters=filters)
 
@@ -78,9 +80,32 @@ class DetailedMonitoringStatusView(APIView):
         project = get_object_or_404(Project, uuid=project_uuid)
         service = HumanSupportDashboardService(project=project)
 
-        filters = request.query_params.copy()
+        filters = {key: value for key, value in request.query_params.items()}
         filters["user_request"] = request.user.email
         data = service.get_detailed_monitoring_status(filters=filters)
+
+        results = data.get("results", [])
+        paginator = LimitOffsetPagination()
+        paginated_results = paginator.paginate_queryset(results, request)
+
+        return paginator.get_paginated_response(paginated_results)
+
+
+class AnalysisDetailedMonitoringStatusView(APIView):
+    permission_classes = [IsAuthenticated, ProjectAuthQueryParamPermission]
+    feature_flag_key = "human-support-detailed-monitoring"
+
+    def get(self, request, *args, **kwargs):
+        project_uuid = request.query_params.get("project_uuid")
+        if not project_uuid:
+            return Response({"detail": "project_uuid is required"}, status=400)
+
+        project = get_object_or_404(Project, uuid=project_uuid)
+        service = HumanSupportDashboardService(project=project)
+
+        filters = {key: value for key, value in request.query_params.items()}
+        filters["user_request"] = request.user.email
+        data = service.get_analysis_detailed_monitoring_status(filters=filters)
 
         results = data.get("results", [])
         paginator = LimitOffsetPagination()
