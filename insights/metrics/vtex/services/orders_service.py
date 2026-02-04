1ec0422d62ca2ecfb8a99a00b562685ed459ae1b
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2026-02-04 10:45:16 -0300

    feat: Integrate JWT token generation in OrdersService for enhanced authentication

--- insights/metrics/vtex/services/orders_service.py
+++ insights/metrics/vtex/services/orders_service.py
@@ -10,6 +10,7 @@ from insights.sources.vtexcredentials.clients import (
     AuthRestClient as VtexAuthClient,
 )
 from insights.sources.vtexcredentials.typing import VtexCredentialsDTO
+from insights.authentication.services.jwt_service import JWTService
 
 logger = logging.getLogger(__name__)
 
@@ -38,7 +39,9 @@ class OrdersService:
             return VtexOrdersRestClient(
                 {
                     "domain": self.project.vtex_account,
-                    "internal_token": self._get_internal_token(),
+                    "internal_token": JWTService().generate_jwt_token(
+                        self.project.uuid
+                    ),
                 },
                 CacheClient(),
                 use_io_proxy=True,
