b4530ef63b8d8b49557618a4303d728956daaf17
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2026-02-20 15:28:54 -0300

    feat: Add tests for the conversational serializer

---
+++ insights/feedback/serializers/tests/test_conversational_serializer.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+
+from insights.feedback.serializers.conversational import (
+    ConversationalFeedbackSerializer,
+)
+
+
+class TestConversationalFeedbackSerializer(TestCase):
+    def test_serializer(self):
+        serializer = ConversationalFeedbackSerializer(
+            data={
+                "TRUST": {"answer": "1", "type": "SCORE_1_5"},
+                "MAKE_DECISION": {"answer": "1", "type": "SCORE_1_5"},
+                "ROI": {"answer": "1", "type": "SCORE_1_5"},
+                "COMMENT": {"answer": "test", "type": "TEXT"},
+            }
+        )
+
+        self.assertTrue(serializer.is_valid())
+
+    def test_serializer_with_invalid_data(self):
+        serializer = ConversationalFeedbackSerializer(
+            data={
+                "TRUST": {"answer": "0", "type": "SCORE_1_5"},
+                "MAKE_DECISION": {"answer": "6", "type": "SCORE_1_5"},
+                "ROI": {"answer": "bla", "type": "SCORE_1_5"},
+                "COMMENT": {"answer": "test" * 256, "type": "TEXT"},
+            }
+        )
+
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(serializer.errors["TRUST"]["answer"][0].code, "invalid_answer")
+        self.assertEqual(
+            serializer.errors["MAKE_DECISION"]["answer"][0].code, "invalid_answer"
+        )
+        self.assertEqual(serializer.errors["ROI"]["answer"][0].code, "invalid_answer")
+        self.assertEqual(
+            serializer.errors["COMMENT"]["answer"][0].code, "invalid_answer"
+        )
