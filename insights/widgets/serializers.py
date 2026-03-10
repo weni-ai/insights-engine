2feffe1b2223ad33e3be8c38be3ec5c3656fd983
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2026-03-10 14:52:17 -0300

    feat: add parent field to WidgetSerializer to include parent widget details in serialization

--- insights/widgets/serializers.py
+++ insights/widgets/serializers.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from rest_framework import serializers
 
 from .models import Widget
@@ -5,7 +6,14 @@ from .models import Widget
 
 class WidgetSerializer(serializers.ModelSerializer):
     is_configurable = serializers.BooleanField(read_only=True)
+    parent = serializers.SerializerMethodField()
 
     class Meta:
         model = Widget
         fields = "__all__"
+
+    def get_parent(self, obj: Widget) -> Optional[dict]:
+        if not obj.parent:
+            return None
+
+        return {"uuid": obj.parent.uuid, "dashboard": obj.parent.dashboard_id}
