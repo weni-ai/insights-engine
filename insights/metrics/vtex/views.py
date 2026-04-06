51de1ad75007e989ac48759d33d6d354e75a2f1b
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2026-04-06 16:10:15 -0300

    refactor: Add missing import for viewsets in views.py
    - Included the import statement for viewsets from rest_framework to ensure proper functionality in the codebase.

--- insights/metrics/vtex/views.py
+++ insights/metrics/vtex/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework import viewsets
 
 from insights.authentication.authentication import JWTAuthentication
 from insights.authentication.permissions import (
