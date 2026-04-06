1248afce286e36b4588652bf1987179f9f1f0ea5
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2026-04-06 16:07:16 -0300

    refactor: Remove unused import in views.py
    - Eliminated the unused import of viewsets from rest_framework to clean up the codebase.

--- insights/metrics/vtex/views.py
+++ insights/metrics/vtex/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
