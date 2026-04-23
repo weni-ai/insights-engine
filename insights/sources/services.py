f6fb0e9e8ffc23caa5d06c5933ef5c70249783ce
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2026-04-23 15:00:55 -0300

    feat: enhance DataSourceService to handle VTEX credentials and improve error handling

--- insights/sources/services.py
+++ insights/sources/services.py
@@ -1,9 +1,19 @@
 from abc import ABC, abstractmethod
 from typing import Type
 
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.exceptions import PermissionDenied
+
+from insights.authentication.services.jwt_service import JWTService
+from insights.projects.parsers import parse_dict_to_json
 from insights.sources.base import BaseQueryExecutor
 from insights.sources.factories import SourceQueryExecutorFactory
+from insights.sources.vtexcredentials.exceptions import VtexCredentialsNotFound
 from insights.widgets.models import Widget
+from insights.widgets.usecases.get_source_data import (
+    cross_source_data_operation,
+    simple_source_data_operation,
+)
 
 
 class BaseDataSourceService(ABC):
@@ -49,5 +59,72 @@ class DataSourceService(BaseDataSourceService):
         filters: dict = {},
         user_email: str = "",
     ):
-        source_query_executor = self.get_source_query_executor(widget.source.slug)
-        # TODO: Implement the logic to get the source data from the widget
+        try:
+            source = widget.source
+            if is_report:
+                widget = widget.report
+
+            source_query_executor = self.get_source_query_executor(source.slug)
+            if source_query_executor is None:
+                raise Exception(
+                    f"could not find a source with the slug {source}, make sure that the widget is configured with a supported source"
+                )
+
+            serialized_auth = {}
+            if widget.type == "vtex_order":
+                if widget.project.vtex_account:
+                    serialized_auth = {
+                        "domain": widget.project.vtex_account,
+                        "internal_token": JWTService().generate_jwt_token(
+                            vtex_account=widget.project.vtex_account
+                        ),
+                    }
+                else:
+                    auth_source = self.get_source_query_executor("vtexcredentials")
+                    try:
+                        serialized_auth: dict = auth_source.execute(
+                            filters={"project": widget.project.uuid},
+                            operation="get_vtex_auth",
+                            parser=parse_dict_to_json,
+                            return_format="",
+                            query_kwargs={},
+                        )
+                    except VtexCredentialsNotFound:
+                        raise PermissionDenied(
+                            detail="VTEX credentials not configured for this project. Please configure the VTEX integration first."
+                        )
+
+            operation_function = (
+                cross_source_data_operation
+                if widget.is_crossing_data
+                else simple_source_data_operation
+            )
+
+            extra_query_kwargs = {}
+
+            if (
+                widget.name == "human_service_dashboard.peaks_in_human_service"
+                and is_report is False
+            ):
+                extra_query_kwargs["timeseries_hour_kwargs"] = {
+                    "start_hour": 7,
+                    "end_hour": 18,
+                }
+
+            return operation_function(
+                widget=widget,
+                source_query=source_query_executor,
+                is_live=is_live,
+                filters=filters,
+                user_email=user_email,
+                auth_params=serialized_auth,
+                extra_query_kwargs=extra_query_kwargs,
+            )
+
+        except ObjectDoesNotExist:
+            raise Exception("Widget not found.")
+
+        except KeyError:
+            raise Exception(
+                "The subwidgets operation needs to be one that returns only one object value."
+            )
